72a5e9d4db1eb1957c1859edec1a78af9201ed55
tanyaofei <tan.yaofei@outlook.com>
2024-08-12 14:09:09 +0800

    ConfigUtils#getEnum

--- devtools-core/src/main/java/io/github/hello09x/devtools/core/config/ConfigUtils.java
+++ devtools-core/src/main/java/io/github/hello09x/devtools/core/config/ConfigUtils.java
@@ -1,7 +1,11 @@
 package io.github.hello09x.devtools.core.config;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author tanyaofei
@@ -21,6 +25,22 @@ public abstract class ConfigUtils {
             return defaultValue;
         }
         return Enum.valueOf(clz, name);
+    }
+
+    public static <E extends Enum<E>> E getEnum(@NotNull FileConfiguration config, @NotNull String key, @NotNull Class<E> type, @Nullable E defaultValue) {
+        var value = config.getString(key);
+        if (value == null || value.isEmpty()) {
+            return defaultValue;
+        }
+
+        try {
+            return Enum.valueOf(type, value);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("The value of %s in config.yml is invalid, allowed values are %s".formatted(
+                    key,
+                    Arrays.stream(type.getEnumConstants()).map(E::name).collect(Collectors.joining(", "))
+            ), e);
+        }
     }
 
 }
